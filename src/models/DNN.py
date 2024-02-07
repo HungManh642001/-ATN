bb0c76d793d8292e429423ec6067104a9539a231
ManhNgoHung <82172471+HungManh642001@users.noreply.github.com>
2024-02-07 10:56:48 +0700

    Update DNN.py

--- src/models/DNN.py
+++ src/models/DNN.py
@@ -27,19 +27,24 @@ class DNN(nn.Module):
     """
     A deep neural network for the reverse process of latent diffusion.
     """
-    def __init__(self, in_dims, out_dims, emb_size, item_size, n_classes=7, time_type="cat", norm=False, act_func='tanh', dropout=0.5):
+    def __init__(self, in_dims, out_dims, emb_size, item_size, gender_classes=2, age_classes=7, hidden_classes=100,time_type="cat", norm=False, act_func='tanh', dropout=0.5):
         super(DNN, self).__init__()
         self.in_dims = in_dims
         self.out_dims = out_dims
         assert out_dims[0] == in_dims[-1], "In and out dimensions must equal to each other."
         self.time_emb_dim = emb_size
         self.item_size = item_size
-        self.n_classes=n_classes
+        self.gender_classes = gender_classes
+        self.age_classes = age_classes
         self.time_type = time_type
         self.norm = norm
+        self.hidden_classes = hidden_classes
 
         self.emb_layer = nn.Linear(self.time_emb_dim, self.time_emb_dim)
-        self.class_emb = EmbedFC(n_classes, self.in_dims[1])
+        self.gender_emb = EmbedFC(gender_classes, self.hidden_classes)
+        self.age_emb = EmbedFC(age_classes, self.hidden_classes)
+
+        self.classes_emb = EmbedFC(self.hidden_classes, self.in_dims[1])
 
         if self.time_type == "cat":
             in_dims_temp = [self.in_dims[0] + self.time_emb_dim] + self.in_dims[1:]
@@ -85,27 +90,40 @@ class DNN(nn.Module):
 
         self.apply(xavier_normal_initialization)
 
-    def forward(self, x, c, class_mask, timesteps):
+    def forward(self, x, gender, age, gender_mask, age_mask, timesteps):
         time_emb = timestep_embedding(timesteps, self.time_emb_dim).to(x.device)
         emb = self.emb_layer(time_emb)
 
-        # convert c to one hot embedding
-        c = nn.functional.one_hot(c.to(torch.int64), num_classes=self.n_classes).type(torch.float)
+        # convert gender to one hot embedding
+        gender = nn.functional.one_hot(gender.to(torch.int64), num_classes=self.gender_classes).type(torch.float)
 
-        # mask out c if class_mask=1
-        class_mask = class_mask[:, None]
-        class_mask = class_mask.repeat(1, self.n_classes)
-        class_mask = (-1*(1 - class_mask))
+        # convert age to one hot embedding
+        age = nn.functional.one_hot(age.to(torch.int64), num_classes=self.age_classes).type(torch.float)
 
-        c = c * class_mask
+        # mask out gender if gender_mask=1
+        gender_mask = gender_mask[:, None]
+        gender_mask = gender_mask.repeat(1, self.gender_classes)
+        gender_mask = (-1*(1 - gender_mask))
+        gender = gender * gender_mask
 
-        c = self.class_emb(c)
+        gender = self.gender_emb(gender)
+
+        # mask out age if gender_mask=1
+        age_mask = age_mask[:, None]
+        age_mask = age_mask.repeat(1, self.age_classes)
+        age_mask = (-1*(1 - age_mask))
+        age = age * age_mask
+
+        age = self.age_emb(age)
+
+        classes = gender*age
+        classes = self.classes_emb(classes)
 
         if self.norm:
             x = F.normalize(x, dim=-1)
         x = self.dropout(x)
 
-        h = torch.cat([x*c, emb], dim=-1)
+        h = torch.cat([x*classes, emb], dim=-1)
         h = self.in_layers(h)
         h = self.out_layers(h)
 
@@ -115,7 +133,7 @@ class DNN(nn.Module):
 
         # h1 = torch.cat([x, emb], dim=-1)
         # h = self.in_layers(h1)
-        # h2 = torch.cat([h*c, h1], dim=-1)
+        # h2 = torch.cat([h*gender, h1], dim=-1)
         # h = self.out_layers(h2)
 
         return h
@@ -154,6 +172,4 @@ def xavier_normal_initialization(module):
         xavier_normal_(module.weight.data)
         if module.bias is not None:
             constant_(module.bias.data, 0)
-
-
 
